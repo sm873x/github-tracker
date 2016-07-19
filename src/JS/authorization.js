bfceab3c679dce11dc5dc918c8b82ac4ec6daec4
Stella Ma <sm873x@gmail.com>
2016-07-19 14:54:00 -0400

    HTML - added message area for error messages. JS - added ajax fail function

--- src/JS/authorization.js
+++ src/JS/authorization.js
@@ -3,7 +3,10 @@
 
     window.tracker = ns = (ns || {});
 
-    $('.logToken').on('submit', function(e){
+    var $alertArea = $('.alert-area');
+    var $logToken = $('.logToken');
+
+    $logToken.on('submit', function(e){
         console.log('start');
         e.preventDefault();
 
@@ -13,12 +16,24 @@
         ns.authorize(token)
             .done(function(data) {
                 console.log('login', data);
-            });
+            })
+            .fail( ns.error(xhr) );
     });
 
+    /**
+     * Use GitHub token to authorize retrieval of user info
+     * @param  {String} token Person Access Token
+     * @return {Promise} jQuery XHR Object containing promise method
+     */
     ns.authorize = function loginAPI(token) {
+        if (!token) {
+            var def = $.Deferred();
+            def.reject('You must have a GitHub Personal Access Token to proceed');
+            return def.promise();
+        }
+
         return $.ajax({
-            url: 'https://api.github.com/user/',
+            url: 'https://api.github.com/user',
             method: 'get',
             headers: {
                 'Authorization': 'token ' + token
@@ -27,4 +42,12 @@
         });
     };
 
+    ns.error = function handleAjaxFail(xhr) {
+        var statCode = xhr.status;
+        if ( 400 <= statCode && statCode < 500 ) {
+            $alertArea.text('Check your token');
+        } else if ( statCode >= 500){
+            $alertArea.text('Ruh roh, looks like we\'re having problems. Check back later please');
+        }
+    };
 })(window.tracker);
