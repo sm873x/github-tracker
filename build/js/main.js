a2dd5827bdb0f887815bfaae990f04d1142c252a
Stella Ma <sm873x@gmail.com>
2016-07-24 23:55:33 -0400

    Added test js for general functions including error and date. Added suite of tests for ns.date.

--- build/js/main.js
+++ build/js/main.js
@@ -153,19 +153,18 @@
 
     window.tracker = ns = ( ns || {} );
 
-
     ns.error = function handleAjaxFail(xhr) {
-        var statCode = xhr.status;
-        if ( 400 <= statCode && statCode < 500 ) {
+        ns.statCode = xhr.status;
+        if ( 400 <= ns.statCode && ns.statCode < 500 ) {
             ns.$alertArea.text('Check your token');
-        } else if ( statCode >= 500){
+        } else if ( ns.statCode >= 500){
             ns.$alertArea.text('Ruh roh, looks like we\'re having problems. Check back later please');
         }
     };
 
     ns.date = function justDate(dateTime){
-        var dateArr = dateTime.split('T');
-        return dateArr[0];
+        ns.dateArr = dateTime.split('T');
+        return ns.dateArr[0];
     };
 
 })(window.tracker);
---
+++ test/specs/test_general.js
@@ -0,0 +1,47 @@
+(function(){
+
+    'use strict';
+
+    var assert = window.chai.assert;
+
+    suite('handleAjaxFail', function() {
+
+        teardown(function(){
+            window.tracker.statCode = '200';
+        });
+
+        test('ensure error function exists', function(){
+            assert.ok(window.tracker.error, 'error function exists');
+            assert.isFunction( window.tracker.error, 'ns.error is a function');
+        });
+
+        test('400 type error results in client side alert', function(){
+            window.tracker.xhr = {status: 400};
+            assert.isObject(window.tracker.$alertArea, '400 alert for client side errors');
+        });
+
+        test('500 type error results in server side alert', function(){
+            window.tracker.xhr = {status: 500};
+            assert.isObject(window.tracker.$alertArea, '500 alert for server side errors');
+        });
+    });
+
+    suite('justDate function gives just month, day and year', function() {
+
+        teardown(function(){
+            window.tracker.dateArr = null;
+        });
+
+        test('ensure date function exists', function(){
+            assert.ok(window.tracker.date, 'date function exists');
+            assert.isFunction( window.tracker.date, 'ns.date is a function');
+        });
+        test('date function works with a valid timestamp', function() {
+            var dateTime = '2016-07-25T03:14:28';
+            window.tracker.dateArr = ['2016-07-25', '03:14:28'];
+            assert.isArray(dateTime.split('T'), 'splitting timestamp on T gives an array of two strings');
+            assert.strictEqual(window.tracker.dateArr[0], '2016-07-25', 'justDate function provides only year, month and day');
+        });
+    });
+
+})();
